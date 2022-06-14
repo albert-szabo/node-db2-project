a94dd0a2b99962fb023f74a3a96b48b5858be896
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-06-14 13:36:40 -0700

    Added migration to create cars table

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  
+  return knex.schema.createTable('cars', table => {
+    table.increments('id');
+    table.string('vin', 17).notNullable().unique();
+    table.string('make', 32).notNullable();
+    table.string('model', 32).notNullable();
+    table.numeric('milage').unsigned().notNullable();
+    table.string('title', 32);
+    table.string('transmission', 32);
+  });
 };
 
 exports.down = function (knex) {
-  
+  return knex.schema.dropTableIfExists('cars');
 };
