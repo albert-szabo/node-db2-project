5863d6a39328c2db06519e62392a7e393ba9d13b
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-06-14 14:27:00 -0700

    Added general GET endpoint to retrieve all cars

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,15 @@
+const database = require('../../data/db-config');
+
 const getAll = () => {
-  // DO YOUR MAGIC
-}
+  return database('cars');
+};
 
 const getById = () => {
   // DO YOUR MAGIC
-}
+};
 
 const create = () => {
   // DO YOUR MAGIC
-}
+};
+
+module.exports = { getAll, getById, create };
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,9 +1,16 @@
 const express = require('express');
 
+const Cars = require('./cars-model');
+
 const router = express.Router();
 
-router.get('/', (request, response, next) => {
-
+router.get('/', async (request, response, next) => {
+    try {
+        const cars = await Cars.getAll();
+        response.json(cars);
+    } catch (error) {
+        next(error);
+    }
 });
 
 router.get('/:id', (request, response, next) => {
