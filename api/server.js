7987298cce67626e383cf4c4124459e37b66066e
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-06-14 14:01:01 -0700

    Added catch-all endpoint and global error-handling middleware

--- api/server.js
+++ api/server.js
@@ -2,6 +2,18 @@ const express = require("express")
 
 const server = express()
 
-// DO YOUR MAGIC
+server.use(express.json());
+
+server.use();
+
+server.use('*', (request, response, next) => {
+    next({ status: 404, message: 'That URL was not found.' });
+});
+
+server.use((error, request, response, next) => {
+    response.status(error.status || 500).json({
+        message: error.message || 'An internal server error occurred.'
+    });
+});
 
 module.exports = server
